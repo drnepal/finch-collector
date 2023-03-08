af5d93d9e821cd89f9f329c7b8d93f0e0ef828e4
devrajn <devraj.nepal@gmail.com>
2023-03-08 00:22:14 -0600

    sign upand auths

--- main_app/views.py
+++ main_app/views.py
@@ -8,6 +8,9 @@ import uuid #python package for creating unique identifiers
 import boto3 #what we'll use to connect to s3
 from django.conf import settings
 
+from django.contrib.auth import login
+from django.contrib.auth.forms import UserCreationForm
+
 
 AWS_ACCESS_KEY= settings.AWS_ACCESS_KEY
 AWS_SECRET_ACCESS_KEY= settings.AWS_SECRET_ACCESS_KEY
@@ -65,6 +68,12 @@ class FinchCreate(CreateView):
     # or, we could redirect to the index page if we want
     # success_url = '/finches'
     # what django recommends, is adding a get_absolute_url method to the model
+    def form_valid(self, form):
+    # Assign the logged in user (self.request.user)
+     form.instance.user = self.request.user  # form.instance is the cat
+    # Let the CreateView do its job as usual
+     return super().form_valid(form)
+
 
 class FinchUpdate(UpdateView):
     model = Finch
@@ -156,3 +165,21 @@ def add_photo(request, finch_id):
             return redirect('detail', finch_id=finch_id)
     # upon success redirect to detail page 
     return redirect('detail', finch_id=finch_id)
+def signup(request):
+    error_message = ''
+    if request.method == 'POST':
+        # This is how to create a 'user' form object
+        # that includes the data from the browser
+        form = UserCreationForm(request.POST)
+        if form.is_valid():
+            # This will add the user to the database
+            user = form.save()
+            # This is how we log a user in via code
+            login(request, user)
+            return redirect('index')
+    else:
+        error_message = 'Invalid sign up - try again'
+        # A bad POST or a GET request, so render signup.html with an empty form
+        form = UserCreationForm()
+    context = {'form': form, 'error_message': error_message}
+    return render(request, 'registration/signup.html', context)
